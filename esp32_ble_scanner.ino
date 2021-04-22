c62083fb7308124bf015545b6fe2b5f622d9bf36
Francesco <ffattori@me.com>
2021-04-22 21:26:28 +0200

    safe rollback commit

--- esp32_ble_scanner.ino
+++ esp32_ble_scanner.ino
@@ -12,7 +12,7 @@
 #include <WiFi.h>
 #include "wifi_config.h"
 
-#define SCAN_LENGTH 30               // scan length
+#define SCAN_LENGTH 10               // scan length
 #define MQTT_NAME "Station1"
 #define TOPIC1 "station1/UUIDs"      // MQTT topic 1
 #define TOPIC2 "station1/totalPeople" //MQTT topic 2
@@ -64,8 +64,6 @@ void loop() {
   BLEScanResults devices = pBLEScan->start(SCAN_LENGTH, false);
   uint8_t total = devices.getCount();
 
-  Serial.println("Devices: " + (String)total);
-
   int SafeSkiingDevice = 0;
   
   for (uint8_t i=0; i<total; i++){
@@ -75,24 +73,45 @@ void loop() {
       SafeSkiingDevice++;
     }
   }
-  char fullmsg[1000];
+  
+  Serial.println("SafeSkiing Devices: " + (String)SafeSkiingDevice);
+  
+  char *fullmsg = (char*)malloc((sizeof(char) * 18 * SafeSkiingDevice) +10);
+  int array_position = 0;
   for (uint8_t i=0; i<total; i++){
       BLEAdvertisedDevice device = devices.getDevice(i);
       String dname = device.getName().c_str();
       
       if (dname =="SafeSkiing"){
-        Serial.println("Found!");
-        std::strcpy(fullmsg, device.getAddress().toString().c_str());
-        Serial.println(fullmsg);
+        const char * addr = device.getAddress().toString().c_str();
+        Serial.println("Found! adding in pos " + (String) addr);
+        for (uint8_t j = 0; j< 17; j++){
+          Serial.print(addr[j]);
+          fullmsg[array_position+j] = addr[j];
+        }
+        Serial.println("");
+        fullmsg[array_position+17] = ',';
+        array_position += 18;
+
+        for (uint8_t k=0; k<=(18 * SafeSkiingDevice); k++){
+          Serial.print(fullmsg[k]);
+        }
+        Serial.println("");
       }
   }
-  
   //publish to topics
-  mqttClient.publish(TOPIC1, fullmsg);
+  if (array_position > 0){
+    mqttClient.publish(TOPIC1, fullmsg);
+  } else {
+    mqttClient.publish(TOPIC1, "");
+  }
+  
+  free(fullmsg);
   
   char msg[7];
   String empty;
   ((String)total).toCharArray(msg, 7);
   mqttClient.publish(TOPIC2, msg);
+  pBLEScan->stop();
   pBLEScan->clearResults();
 }
